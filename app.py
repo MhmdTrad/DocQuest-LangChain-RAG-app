0c0b16169013ae5f2c3dfbcdd438c1eea6cbff2a
MhmdTrad <106621320+MhmdTrad@users.noreply.github.com>
2024-03-03 13:47:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from dotenv import load_dotenv
 import streamlit as st
 import os
-from htmlTemplates import css, bot_template, background_css, page_bg_img, footer
-from pdf_utils import process_pdf_and_get_vectorstore, process_question, PrepChunks
+from htmlTemplates import css, bot_template, page_bg_img, footer
+from pdf_utils import process_pdf,get_vectorstore, process_question, PrepChunks
 
 
 def main():
@@ -21,19 +21,24 @@ def main():
     # Extract the text
     if pdf is not None:
         # Create the chunks then the  embeddings
-        knowledge_base = process_pdf_and_get_vectorstore(pdf_file=pdf)
-        greeting ="Hi, I'm DocQuest your assistant, happy to meet you"
-        st.write(bot_template.replace("{{MSG}}", greeting), unsafe_allow_html=True)
-        # show user input
-        user_question = st.text_input("Ask a question about your PDF:")
-        if user_question:  # Check if the user has provided a question
-            response = process_question(user_question, knowledge_base)
+        chunks = process_pdf(pdf)
+        if chunks:
+            vectorstore = get_vectorstore(chunks)
 
-            st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
+            greeting ="Hi, I'm DocQuest your assistant, happy to meet you"
+            st.write(bot_template.replace("{{MSG}}", greeting), unsafe_allow_html=True)
+            # show user input
+            user_question = st.text_input("Ask a question about your PDF:")
+            if user_question:  # Check if the user has provided a question
+                response = process_question(user_question, vectorstore)
 
+                st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
+        else:
+            handling_error = "Please try differnet PDF contains texts 😊"
+            st.write(bot_template.replace("{{MSG}}", handling_error), unsafe_allow_html=True)
+            pass
 if __name__ == '__main__':
     main()
-
 
 st.markdown(footer, unsafe_allow_html=True)
 
