0c22ff28717a11e925f7679122d0e06551d4189c
MhmdTrad <106621320+MhmdTrad@users.noreply.github.com>
2024-03-03 16:09:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 from dotenv import load_dotenv
 import streamlit as st
-from streamlit.server.server import Server
+import os
 from htmlTemplates import css, bot_template, page_bg_img, footer
-from pdf_utils import process_pdf, get_vectorstore, process_question, PrepChunks
+from pdf_utils import process_pdf,get_vectorstore, process_question, PrepChunks
 
-def get_session_id():
-    ctx = st.report_thread.get_report_ctx()
-    this_session_id = None
-    current_server = Server.get_current()
-    if hasattr(current_server, "_session_info"):
-        this_session_id = current_server._session_info.id
-    return this_session_id
 
 def main():
-    session_id = get_session_id()
-
     load_dotenv()
     # Apply the HTML and JavaScript
     st.set_page_config(page_title="Ask your PDF 🦜️🔗")
 
-    st.write(css, unsafe_allow_html=True)
+    st.write(css,unsafe_allow_html=True)
     st.markdown(page_bg_img, unsafe_allow_html=True)
     # st.header("Ask your PDF 💬 ")
     st.markdown("<h1 class='title'>DocQuest 💬</h1>", unsafe_allow_html=True)
@@ -43,10 +34,9 @@ def main():
 
                 st.write(bot_template.replace("{{MSG}}", response), unsafe_allow_html=True)
         else:
-            handling_error = "Please try different PDF contains texts 😊"
+            handling_error = "Please try differnet PDF contains texts 😊"
             st.write(bot_template.replace("{{MSG}}", handling_error), unsafe_allow_html=True)
             pass
-
 if __name__ == '__main__':
     main()
 
